7c56ae007a88947f44cbcd0e987fea18ad41f497
alunos.fiap <alunos.fiap@outlook.com>
2024-07-27 16:56:15 -0300

    test: created video repository test

---
+++ application/repositories/video_repository_test.go
@@ -0,0 +1,34 @@
+package repositories_test
+
+import (
+	"encoder/application/repositories"
+	"encoder/domain"
+	"encoder/framework/database"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestNewVideoRepositoryDbInsert(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+	v, err := repo.Find(video.ID)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.NotEmpty(t, video.ID)
+	require.Equal(t, video.ID, v.ID)
+	require.Equal(t, video.FilePath, v.FilePath)
+
+}
