8d5cb6ed13c260d7d78bd565a1c4e87acbc3472c
alunos.fiap <alunos.fiap@outlook.com>
2024-07-27 18:20:51 -0300

    test: created Update test

--- application/repositories/job_repository_test.go
+++ application/repositories/job_repository_test.go
@@ -29,10 +29,36 @@ func TestJobRepositoryDbInsert(t *testing.T) {
 	repoJob.Insert(job)
 
 	j, err := repoJob.Find(job.ID)
-	require.Nil(t, err)
 	require.NotEmpty(t, j.ID)
-	require.Equal(t, job.ID, j.ID)
-	require.Equal(t, job.VideoID, video.ID)
-	require.Equal(t, job.Video, j.Video)
-	require.Equal(t, job.OutputBucketPath, j.OutputBucketPath)
+	require.Nil(t, err)
+	require.Equal(t, j.ID, job.ID)
+	require.Equal(t, j.VideoID, video.ID)
+}
+
+func TestJobRepositoryDbUpdate(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	job, err := domain.NewJob("output_path", "Pending", video)
+	require.Nil(t, err)
+
+	repoJob := repositories.JobRepositoryDb{Db: db}
+	repoJob.Insert(job)
+
+	job.Status = "Complete"
+
+	repoJob.Update(job)
+
+	j, err := repoJob.Find(job.ID)
+	require.NotEmpty(t, j.ID)
+	require.Nil(t, err)
+	require.Equal(t, j.Status, job.Status)
 }
